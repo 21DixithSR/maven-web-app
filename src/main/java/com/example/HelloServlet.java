4c90324615d4f77685d41ce9b07e22f3aa3d9736
21DixithSR <83853495+21DixithSR@users.noreply.github.com>
2025-10-18 20:57:08 +0530

    Update HelloServlet.java

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,8 +1,9 @@
 package com.example;
 
 import java.io.*;
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
 
 public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
